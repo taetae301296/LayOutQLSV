8c0fe09b312b6888cb8696d9937bcc120d2f3c3a
Hiep0502 <dthiep.gm@gmail.com>
2019-05-11 17:31:51 -0700

    first commit

---
+++ app/src/main/java/com/example/quanlysinhvien/Diem_Edit.java
@@ -0,0 +1,13 @@
+package com.example.quanlysinhvien;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Diem_Edit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_diem__edit );
+    }
+}
---
+++ app/src/main/java/com/example/quanlysinhvien/Edit_MonHoc.java
@@ -0,0 +1,15 @@
+package com.example.quanlysinhvien;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Edit_MonHoc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_edit__mon_hoc );
+
+    }
+}
